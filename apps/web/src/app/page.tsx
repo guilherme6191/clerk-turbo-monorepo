1e98a2c7a14ab190cf645442d56859ff2d93da39
guilherme6191 <guilherme@clerk.dev>
2025-05-20 12:21:43 -0300

    Setup

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { PricingTable } from "@repo/ui/client";
+import { PricingTableComponent } from "@repo/ui/pricing-table";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex gap-[32px] row-start-2 items-center sm:items-start w-full">
+        <PricingTableComponent />
+        <PricingTable />
+      </main>
+    </div>
+  );
+}
---
+++ packages/ui/src/pricing-table.tsx
@@ -0,0 +1,10 @@
+import { PricingTable } from "@clerk/nextjs";
+
+export function PricingTableComponent() {
+  return (
+    <div className="w-full">
+      <h1>Pricing Table</h1>
+      <PricingTable />
+    </div>
+  );
+}
